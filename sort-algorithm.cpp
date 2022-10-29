54eb005fc6ea5e442ff45dcdf63ce46cae0ea506
LinhNC <linhnc184@gmail.com>
2022-10-29 11:21:11 +0700

    insertion sort

--- sort-algorithm.cpp
+++ sort-algorithm.cpp
@@ -2,12 +2,9 @@
 
 using namespace std;
 
-template <unsigned int size> void printArray(int (&nums)[size]) {
-  for (int i = 0; i < size; i++) {
-    cout << nums[i];
-    if (i < size - 1) {
-      cout << ",";
-    }
+void printArray(int nums[], int n) {
+  for (int i = 0; i < n; i++) {
+    cout << nums[i] << " ";
   }
   cout << endl;
 }
@@ -18,53 +15,63 @@ void swap(int *n1, int *n2) {
   *n1 = *n1 - *n2;
 }
 
-template<unsigned int size> void selectionSort(int (&nums)[size]) {
-  cout << endl << "Selection Sort: ";
+void selectionSort(int nums[], int n) {
+  for (int i = 0; i < n - 1; i++) {
+    int k = i;
 
-  for (int i = 0; i < size - 1; i++) {
-    int s_index = i;
-    // lần đầu tiên số bé nhất đã được đưa lên đầu
-    for (int j = i + 1; j < size; j++) {
-      if (nums[j] < nums[s_index]) {
-        s_index = j;
+    for (int j = i + 1; j < n; j++) {
+      if (nums[j] < nums[k]) {
+        k = j;
       }
     }
-    swap(nums[s_index], nums[i]);
+
+    swap(nums[k], nums[i]);
   }
 
-  printArray(nums);
+  cout << "Selection Sort: ";
+  printArray(nums, n);
 }
 
-template<unsigned int size> void bubbleSort(int (&nums)[size]) {
-  cout << endl << "Bubble Sort: ";
-
-  for (int i = 0; i < size - 1; i++) {
-    // lần đầu tiên số lớn nhất đã được xếp xuống dưới cùng
-    for (int j = 0; j < size - i - 1; j++) {
+void bubbleSort(int nums[], int n) {
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
       if (nums[j] > nums[j + 1]) {
         swap(nums[j], nums[j + 1]);
       }
     }
   }
 
-  printArray(nums);
+  cout << "Bubble Sort: ";
+  printArray(nums, n);
 }
 
-template<unsigned int size> void insertionSort() {
-  
+void insertionSort(int nums[], int n) {
+  int i, j;
+
+  for (i = 1; i < n; i++) {
+    j = i - 1;
+
+    while (j >= 0 && (nums[j + 1] < nums[j])) {
+      swap(nums[j + 1], nums[j]);
+      j = j - 1;
+    }
+  }
+
+  cout << "Insertion Sort: ";
+  printArray(nums, n);
 }
 
 int main() {
-  int nums[] = {8,3,7,9,5,4,2,1,10};
+  int nums[10] = {8,3,7,10,9,5,4,6,2,1};
 
   cout << endl << endl;
   cout << "Initial array: ";
-  printArray(nums);
+  printArray(nums, 10);
   cout << endl << endl;
 
-  selectionSort(nums);
-
-  bubbleSort(nums);
+  // selectionSort(nums, 10);
+  // bubbleSort(nums, 10);
+  insertionSort(nums , 10);
 
   return 0;
 }
