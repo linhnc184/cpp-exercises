332c5145a7515ea461571561403400a31fa20e43
LinhNC <linhnc184@gmail.com>
2022-10-30 14:46:28 +0700

    quick sort with the last element as pivot

--- sort-algorithm.cpp
+++ sort-algorithm.cpp
@@ -61,6 +61,34 @@ void insertionSort(int nums[], int n) {
   printArray(nums, n);
 }
 
+int quickSortPartition(int nums[], int first, int last) {
+  // lấy số cuối cùng làm 'pivot'
+  int pivot = nums[last];
+  int j = first - 1;
+
+  // đưa số bé hơn 'pivot' sang bên trái
+  for (int i = first; i < last; i++) {
+    if (nums[i] < pivot) {
+      j++;
+      swap(nums[j], nums[i]);
+    }
+  }
+
+  // đưa 'pivot' vào giữa những số bé hơn và lớn hơn nó
+  swap(nums[j + 1], nums[last]);
+
+  // trả về index của 'pivot' để tiếp tục (đệ quy) sắp xếp mảng con
+  return j + 1;
+}
+
+void quickSort(int nums[], int first, int last) {
+  if (first < last) {
+    int partition_i = quickSortPartition(nums, first, last);
+    quickSort(nums, first, partition_i - 1);
+    quickSort(nums, partition_i + 1, last);
+  }
+}
+
 int main() {
   int nums[10] = {8,3,7,10,9,5,4,6,2,1};
 
@@ -71,7 +99,11 @@ int main() {
 
   // selectionSort(nums, 10);
   // bubbleSort(nums, 10);
-  insertionSort(nums , 10);
+  // insertionSort(nums, 10);
+
+  quickSort(nums, 0, 9);
+  cout << "Quick Sort: ";
+  printArray(nums, 10);
 
   return 0;
 }
